d4c0688e4d35ce55a016fdc2f890e2ea8cacd96e
Hiroshi Nakai <hrsi.ad@gmail.com>
2025-01-12 22:09:05 +0900

    CREATE作成

--- server.js
+++ server.js
@@ -1,7 +1,26 @@
+const { PrismaClient } = require("@prisma/client");
 const express = require("express");
 const app = express();
 const PORT = 8000;
+const prisma = new PrismaClient();
+
+// post JSON Format.
+app.use(express.json())
+
+app.post("/", async (req, res) => {
+  const { title, body } = req.body; // from postMan
+
+  // モデル名を小文字でデータ挿入。
+  const posts = await prisma.posts.create({
+    data: {
+      title: title,
+      bode: body,
+    },
+  });
+
+  return res.json(posts);
+});
 
 app.listen(PORT, () => {
-    console.log("サーバーが起動中....")
+  console.log("サーバーが起動中....");
 });
